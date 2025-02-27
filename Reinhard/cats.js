6ecc033769e43556cf31a2da483ea5ce01fce746
reinh <Reinhardvandenbulcke@hotmail.com>
2025-02-27 17:32:22 +0100

    oplossing server

---
+++ Reinhard/cats.js
@@ -0,0 +1,40 @@
+const cats = [
+    {
+      id: 1,
+      name: "Whiskers",
+      breed: "Persian",
+      age: 5,
+      color: "Gray",
+      favoriteFood: "Tuna",
+      adopted: true
+    },
+    {
+      id: 2,
+      name: "Mittens",
+      breed: "Maine Coon",
+      age: 3,
+      color: "Brown Tabby",
+      favoriteFood: "Chicken",
+      adopted: false
+    },
+    {
+      id: 3,
+      name: "Fluffy",
+      breed: "Ragdoll",
+      age: 2,
+      color: "Blue Bicolor",
+      favoriteFood: "Salmon",
+      adopted: true
+    },
+    {
+      id: 4,
+      name: "Shadow",
+      breed: "British Shorthair",
+      age: 4,
+      color: "Black",
+      favoriteFood: "Beef",
+      adopted: false
+    }
+  ];
+  
+  module.exports = cats;
