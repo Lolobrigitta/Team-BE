7a602001953d40ade1addc5d09c04cc8625e080b
Jelle Vansteenkiste <jelle.vansteenkiste@student.vives.be>
2025-02-27 16:21:50 +0100

    fruit list

---
+++ Jelle/Fruit.js
@@ -0,0 +1,8 @@
+const fruits = [
+    { id: 1, name: 'Apple', color: 'Red', family: 'Rosaceae', origin: 'Central Asia' },
+    { id: 2, name: 'Banana', color: 'Yellow', family: 'Musaceae', origin: 'Southeast Asia' },
+    { id: 3, name: 'Orange', color: 'Orange', family: 'Rutaceae', origin: 'Southeast Asia' },
+    { id: 4, name: 'Mango', color: 'Yellow', family: 'Anacardiaceae', origin: 'South Asia' },
+    { id: 5, name: 'Grapes', color: 'Green or Purple', family: 'Vitaceae', origin: 'Near East' },
+    { id: 6, name: 'Pineapple', color: 'Yellow', family: 'Bromeliaceae', origin: 'South America' }
+];
