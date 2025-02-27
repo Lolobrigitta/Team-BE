86fd1536fb1b73942012e30548b09a3abbfb475b
EdouardGosseye <edouard.gosseye2@student.vives.be>
2025-02-27 16:46:37 +0100

    require /products in server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const port = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+const data = require('./products');
 
 app.set("view engine", "handlebars");
 
